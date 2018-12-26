6f1a9cd0ffbb7960a538ab7f11362b7c5fb83c1f
Steve <cordovas@bc.edu>
2018-12-26 14:12:56 -0500

    w3

--- resources/views/messagesShow.blade.php
+++ resources/views/messagesShow.blade.php
@@ -12,14 +12,19 @@
                     <span class="badge badge-pill badge-info">{{ $tag->name }}</span>
                 @endforeach
             </p>
-            <form action='/messages/{{$message->id}}/delete' method='POST'>
+            <form action='/messages/{{$message->id}}/delete' method='POST' class='alinks'>
                 {{method_field('delete') }}
                 {{ csrf_field() }}
                 <input type='submit' class="btn btn-secondary btn-sm " Value='Delete'>
 
-                <a class='alinks' href='/messages/{{ $message->id }}/edit'><button type="button" class="btn btn-primary btn-sm">Edit</button></a>
+                {{--<a class='alinks' href='/messages/{{ $message->id }}/edit'><button type="button" class="btn btn-primary btn-sm">Edit</button></a>--}}
 
             </form>
+
+            <form action='/messages/{{$message->id}}/edit' method='GET' class='alinks' >
+                <input type='submit' class="btn btn-primary btn-sm " Value='Edit'>
+            </form>
+
         </div>
     </div>
 
